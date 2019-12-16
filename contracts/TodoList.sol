b34ec271fcd8781d0a61eafa006ae1da6728d5a3
William <liams_1998@hotmail.com>
2019-12-16 02:02:15 +0200

    added render to index.html + connection to ganache

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completeted;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Create your first blockchain app");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false); 
+    }
 }
